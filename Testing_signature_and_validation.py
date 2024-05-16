61e133d8defbf903880c05cc6c60dbd2050e3a17
Hammad Ahmed <101525413+hammad-ahmed-01@users.noreply.github.com>
2024-05-16 22:44:12 +0500

    Update Testing_signature_and_validation.py

--- Testing_signature_and_validation.py
+++ Testing_signature_and_validation.py
@@ -1,10 +1,9 @@
 import falcon
 
 # Step 1: Key Generation
-sk = falcon.SecretKey(8)  # Generate a private key with parameter n = 512
+sk = falcon.SecretKey(8)  # use 2,4,8,16,32,62,128,256,512,1024
 pk = falcon.PublicKey(sk)   # Generate the corresponding public key from the private key
 
-# print('- initialize a secret key for:\n- n = 128, 256, 512, 1024,\n- phi = x ** n + 1,\n- q = 12 * 1024 + 1\n- find a preimage t of a point c (both in ( Z[x] mod (Phi,q) )**2 ) such that t*B0 = c\n- hash a message to a point of Z[x] mod (Phi,q)\n- sign a message\n- verify the signature of a message')
 print("\n\n", sk,
       "\nPublic key:", pk)
 # Step 2: Signing a Message
